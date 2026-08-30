e2ab3d82e289daa05d8f469798373c328977b766
steven-gold <proorangeone@gmail.com>
2026-08-31 04:55:26 +0800

    reset WB-01 page entry to VIS-01 visual freeze

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import { DashboardVisual } from "@/components/pages/DashboardVisual";
-import { AppShell } from "@/components/shell/AppShell";
-import { DASHBOARD_SECTION_KEYS } from "@/domain/dashboard/readModelContract";
 
 export default function Home() {
-  if (DASHBOARD_SECTION_KEYS.length !== 14) {
-    throw new Error("WB-01 dashboard section runtime binding incomplete");
-  }
-
-  return (
-    <AppShell activeNavId="NAV-01">
-      <DashboardVisual />
-    </AppShell>
-  );
+  return <DashboardVisual />;
 }
